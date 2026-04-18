e263b85b17c1593f024b4abe8738d784ec352205
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-18 19:25:28 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,1 +1,21 @@
+const categories = [
+  {
+    name: "Shoes",
+    image: "assets/shoes.jpg",
+    id: "shoes"
+  },
+  {
+    name: "Tech",
+    image: "assets/tech.jpg",
+    id: "tech"
+  }
+];
 
+const products = {
+  shoes: [
+    { name: "Nike Air", image: "assets/product1.jpg" }
+  ],
+  tech: [
+    { name: "iPhone", image: "assets/product2.jpg" }
+  ]
+};
